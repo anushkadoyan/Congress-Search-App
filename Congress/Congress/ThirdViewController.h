2881224401bb30a70fc5b159435f9f7e3f6e2ac0
Anush Kadoyan <anushkadoyan@gmail.com>
2016-11-19 21:44:53 -0800

    Added cocoa pods

---
+++ Congress/Congress/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  Congress
+//
+//  Created by Anush on 11/19/16.
+//  Copyright © 2016 Anush Kadoyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
---
+++ Congress/Pods/Target Support Files/Pods-CongressUITests/Pods-CongressUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_CongressUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CongressUITestsVersionString[];
+
